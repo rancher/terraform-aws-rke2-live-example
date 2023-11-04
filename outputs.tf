5522fca239d451a62cca8ca1e2f3daea999929e0
Matt Trachier <matttrach@gmail.com>
2023-11-03 19:42:32 -0500

    fix: make future ssh command sensitive (#15)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,6 @@ output "kubeconfig" {
   sensitive = true
 }
 output "ssh" {
-  value = "${local.username}@${module.aws_rke2_rhel9_rpm.server_public_ip}"
+  value     = "${local.username}@${module.aws_rke2_rhel9_rpm.server_public_ip}"
+  sensitive = true
 }
