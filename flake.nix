b99dacec5f8fb3b2f3d20dea81713c88ea63de03
Matt Trachier <matttrach@gmail.com>
2023-11-13 16:37:15 -0600

    fix: encrypt and decrypt state in ci (#24)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -19,26 +19,30 @@
   #inputs.nixpkgs.follows = "nixpkgs/0228346f7b58f1a284fdb1b72df6298b06677495";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-terraform.url = "github:/NixOS/nixpkgs/39ed4b64ba5929e8e9221d06b719a758915e619b";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nixpkgs-terraform, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       # 'legacy' is not bad, it looks for previously imported nixpkgs
       #  this allows idempotent loading of nixpkgs in dependent flakes
       # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/8
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-terraform = nixpkgs-terraform.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; with pkgs-terraform; [
+          buildInputs = with pkgs; [
             bashInteractive
-            terraform
+            tfswitch
             sops
             age
           ];
+          shellHook = ''
+            rm -rf "/usr/local/bin/switched-terraform"
+            install -d "/usr/local/bin/switched-terraform"
+            tfswitch -b "/usr/local/bin/switched-terraform/terraform" -d "1.5.7" 1.5.7 > /dev/null;
+            export PATH="$PATH:/usr/local/bin/switched-terraform";
+          '';
         };
       }
     );
