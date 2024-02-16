16a9c3ee60cfee38520d0a1c8c2d6722a5dedc41
Matt Trachier <matttrach@gmail.com>
2024-02-15 18:45:04 -0600

    fix: output the proper info for image user and id (#151)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- phase_3_prototype/outputs.tf
+++ phase_3_prototype/outputs.tf
@@ -1,10 +1,10 @@
 output "image_id" {
-  value = local.name
+  value = aws_ami_from_instance.golden.id
 }
 output "image_admin_group" {
   value = "wheel" # got this from rhel-9 type https://github.com/rancher/terraform-aws-server/blob/main/modules/image/types.tf
 }
-output "image_initial_user" {
+output "image_user" {
   value = local.username
 }
 output "image_workfolder" {
