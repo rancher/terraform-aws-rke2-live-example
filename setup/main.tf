cb40834bcbfb295fe441250699b03871642ca695
Matt Trachier <matttrach@gmail.com>
2024-02-20 15:28:03 -0600

    fix: setup (#34)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- setup/main.tf
+++ setup/main.tf
@@ -21,9 +21,9 @@ import {
 }
 
 resource "github_repository" "this" {
-  name                        = local.name
-  description                 = local.description
-  visibility                  = "public"
+  name        = local.name
+  description = local.description
+  #visibility                  = "public" # only change visibility in repos that belong to an org
   has_issues                  = false
   has_discussions             = false
   has_projects                = false
@@ -46,29 +46,29 @@ resource "github_branch" "main" {
 }
 
 # branch protections are only for enterprise and public repos
-resource "github_branch_protection" "main" {
-  depends_on = [
-    github_repository.this,
-    github_branch.main,
-  ]
-  repository_id = github_repository.this.name
+# resource "github_branch_protection" "main" {
+#   depends_on = [
+#     github_repository.this,
+#     github_branch.main,
+#   ]
+#   repository_id = github_repository.this.name
 
-  pattern                 = github_branch.main.branch
-  enforce_admins          = true
-  require_signed_commits  = true
-  required_linear_history = true
-  allows_deletions        = false # protect branch from deletion
+#   pattern                 = github_branch.main.branch
+#   enforce_admins          = true
+#   require_signed_commits  = true
+#   required_linear_history = true
+#   allows_deletions        = false # protect branch from deletion
 
-  required_pull_request_reviews {
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
-    dismiss_stale_reviews           = true
-  }
+#   required_pull_request_reviews {
+#     require_code_owner_reviews      = true
+#     required_approving_review_count = 1
+#     dismiss_stale_reviews           = true
+#   }
 
-  required_status_checks {
-    strict = true # require the latest push on the branch to be reviewed
-  }
-}
+#   required_status_checks {
+#     strict = true # require the latest push on the branch to be reviewed
+#   }
+# }
 
 resource "github_branch_default" "main" {
   depends_on = [
